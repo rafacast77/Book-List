6c1310dd87dd1da03162b2af9e686a39ad8b3062
rafacast77 <1600389@rgu.ac.uk>
2020-06-28 18:43:18 +0100

    Placeholders for bookings and members done, tab navigation works

--- util.js
+++ util.js
@@ -2,13 +2,23 @@
 
 const card = document.querySelector('.card'),
   tab_EL = document.querySelector('.tab-container'),
-  tab_li_ELS = document.querySelector('#tab-ul').children;
+  tabs_ELs = document.querySelectorAll('[data-tabs-target]'),
+  tabContent_ELs = document.querySelectorAll('[data-tab-content');
 
-tab_EL.addEventListener('click', function (e) {
-  for (var x of tab_li_ELS) {
-    x.className = '';
-  }
-  e.target.parentElement.className = 'selected';
+tabs_ELs.forEach((tab) => {
+  tab.addEventListener('click', function (e) {
+    // Re-selects new tab
+    tabs_ELs.forEach((tabEl) => {
+      tabEl.className = '';
+    });
+    e.target.parentElement.className = 'selected';
+    //Re-selects new content
+    tabContent_ELs.forEach((tabTarget) => {
+      tabTarget.classList.remove('active');
+    });
+    const target = document.querySelector(tab.dataset.tabsTarget);
+    target.className ='active';
+  });
 });
 
 function toastAlert(message, state) {
